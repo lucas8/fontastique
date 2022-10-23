b67644b3ce2e02d2da8315fc5f4024ce35483276
lucas <lucas.stettner@gmail.com>
2022-10-23 17:16:01 -0400

    fix typecheck

--- apps/client/lint-staged.config.mjs
+++ apps/client/lint-staged.config.mjs
@@ -8,17 +8,7 @@ const config = {
    * @param {string[]} filenames
    * @return {string[]}
    */
-  '{package-lock.json,packages/**/{*.ts,*.tsx,tsconfig.json}}': filenames => {
-    // if dependencies was changed run type checking for all packages
-    if (filenames.some(f => f.endsWith('package-lock.json'))) {
-      return ['yarn typecheck'];
-    }
-
-    // else run type checking for staged packages
-    const fileNameToPackageName = filename =>
-      filename.replace(resolve(process.cwd(), 'packages') + sep, '').split(sep)[0];
-    return [...new Set(filenames.map(fileNameToPackageName))].map(p => `yarn typecheck:${p}`);
-  },
+  '{package-lock.json,packages/**/{*.ts,*.tsx,tsconfig.json}}': ['yarn typecheck'],
 };
 
 export default config;
