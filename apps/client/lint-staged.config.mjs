388261d15c21b3c8546fde1f80013381da467ef4
lucas <lucas.stettner@gmail.com>
2022-10-23 20:35:57 -0400

    convert turborepo back to single repo

--- apps/client/lint-staged.config.mjs
+++
@@ -1,6 +0,0 @@
-const config = {
-  '*.{js,mjs,cjs,ts,mts,cts,tsx}': ['eslint --cache --fix', 'prettier --write --ignore-unknown'],
-  '{package-lock.json,packages/**/{*.ts,*.tsx,tsconfig.json}}': ['yarn typecheck'],
-};
-
-export default config;
