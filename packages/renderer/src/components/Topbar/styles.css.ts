3313d72c02be12b27403d8b37ca156f54269b170
lucas <lucas.stettner@gmail.com>
2022-10-25 00:36:35 -0400

    add title animation

--- packages/renderer/src/components/Topbar/styles.css.ts
+++ packages/renderer/src/components/Topbar/styles.css.ts
@@ -1,7 +1,12 @@
-import { style } from '@vanilla-extract/css';
+import { keyframes, style } from '@vanilla-extract/css';
 import { sprinkles, vars } from '~/styles';
 
 export const TOPBAR_HEIGHT = 33;
+
+const animateUp = keyframes({
+  '0%': { opacity: '0', transform: 'translateY(-10px)' },
+  '100%': { opacity: '1', transform: 'none' },
+});
 
 export const container = style({
   width: '100%',
@@ -15,7 +20,12 @@ export const container = style({
   backgroundColor: vars.colors.background,
 });
 
-export const title = sprinkles({
-  fontWeight: 'bold',
-  mr: 1,
-});
+export const title = style([
+  sprinkles({
+    fontWeight: 'bold',
+    mr: 1,
+  }),
+  style({
+    animation: `420ms ease 0ms 1 normal backwards running ${animateUp}`,
+  }),
+]);
