0b060a5d2207769a3aa053c638bf3d1a040e008c
lucas <lucas.stettner@gmail.com>
2022-11-17 16:20:27 -0500

    builder changes

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -1,24 +1,54 @@
-if (process.env.VITE_APP_VERSION === undefined) {
-  const now = new Date();
-  process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${
-    now.getUTCHours() * 60 + now.getUTCMinutes()
-  }`;
-}
-
 /**
  * @type {import('electron-builder').Configuration}
  * @see https://www.electron.build/configuration/configuration
  */
 const config = {
+  appId: 'com.lucasstettner.typescape',
   productName: 'Typescape',
   directories: {
     output: 'dist',
     buildResources: 'buildResources',
   },
-  files: ['packages/**/dist/**'],
-  extraMetadata: {
-    version: process.env.VITE_APP_VERSION,
+  // afterSign: async context => {
+  //   // Mac releases require hardening+notarization: https://developer.apple.com/documentation/xcode/notarizing_macos_software_before_distribution
+  //   if (!isDebug && context.electronPlatformName === 'darwin') {
+  //     await notarizeMac(context);
+  //   }
+  // },
+  dmg: {
+    background: null,
+    backgroundColor: '#DFD0BF',
+    window: {
+      width: 400,
+      height: 300,
+    },
+    contents: [
+      {
+        x: 100,
+        y: 100,
+      },
+      {
+        x: 300,
+        y: 100,
+        type: 'link',
+        path: '/Applications',
+      },
+    ],
   },
+  mac: {
+    target: 'dmg',
+    category: 'public.app-category.utilities',
+    hardenedRuntime: true,
+    // gatekeeperAssess: true,
+  },
+  win: {
+    target: 'nsis',
+  },
+  linux: {
+    target: 'AppImage',
+    category: 'Utility',
+  },
+  files: ['packages/**/dist/**'],
 };
 
 module.exports = config;
