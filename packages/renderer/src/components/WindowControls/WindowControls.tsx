e153f072d2de851c2f4fb1530cb462875c309f20
lucas <lucas.stettner@gmail.com>
2022-11-17 22:27:30 -0500

    test auto-updating

--- packages/renderer/src/components/WindowControls/WindowControls.tsx
+++ packages/renderer/src/components/WindowControls/WindowControls.tsx
@@ -5,6 +5,7 @@ import * as styles from './styles.css';
 // TODO: fix this for windows/linux
 export const WindowControls = () => (
   <Box className={styles.container}>
+    <Box display="none">test</Box>
     {new Array(3).fill(true).map((_, i) => (
       <Box key={i} className={styles.trafficLight} />
     ))}
